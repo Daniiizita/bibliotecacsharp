ec1d878f7a846003ec93f797b14de0f993325e28
Danielle <goncalvesdani54@hotmail.com>
2024-05-22 00:23:49 -0300

    criando os arquivos base do sistema

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/* Deve conter as seguintes:
+    - Classes: Livro, Usuario, Biblioteca, Emprestimo.
+    - 1 arquivo pra cada classe e dps importar pelo namespace (SistemaBiblioteca) pro arquivo program.cs
+    -
+*/
 Console.WriteLine("Hello, World!");
